de26dbfaf5e5e46c2f29e124f09eda4e972590a7
kamlesh70 <kamalmehra3571@gmail.com>
2025-06-30 20:52:18 +0530

    add jest mock

--- src/views/Home-view.vue
+++ src/views/Home-view.vue
@@ -1,5 +1,9 @@
 <template>
   <h1>This is TO-DO application</h1>
+  <p id="test">{{ test }}</p>
 </template>  
-<script setup></script>
+<script setup>
+import { sum } from '@/utils/test';
+const test = sum(1, 2);
+</script>
 <style scoped></style>
--- tests/unit/view/Home.spec.js
+++ tests/unit/view/Home.spec.js
@@ -1,13 +1,23 @@
+import { sum } from "@/utils/test";
 import HomeView from "@/views/Home-view.vue";
 import { mount } from "@vue/test-utils"
 
+jest.mock('@/utils/test');
+
 describe("Home View", () => {
 
+  const sumMock = sum;
+  sumMock.mockReturnValue(10);
   const wrapper = mount(HomeView);
 
-  it('should show heading', () => {
+  it('should show heading', async () => {
+
     const heading = wrapper.find('h1');
     expect(heading.exists()).toBe(true);
     expect(heading.text()).toBe("This is TO-DO application");
+    const para = wrapper.find('#test');
+    expect(para.text()).toBe("10");
+
+    expect(sumMock).toHaveBeenCalled();
   });
 })
